7fc41bb65ab6329bc01fbd9329ab35a563e033ed
Robert Zondervan <rjzondervan@gmail.com>
2020-04-23 15:53:37 +0200

    Fixtures for huwelijksplanner

--- api/src/DataFixtures/AppFixtures.php
+++ api/src/DataFixtures/AppFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Organization;
+use App\Entity\Service;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
@@ -21,10 +23,21 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         // Lets make sure we only run these fixtures on larping enviroment
-        if (strpos($this->params->get('app_domain'), "conduction.nl") == false) {
+        if (strpos($this->params->get('app_domain'), "huwelijksplanner.online") == false && $this->params->get('app_domain' != 'huwelijksplanner.online')) {
             return false;
         }
 
+        $organization = new Organization();
+        $organization->setRsin('002220647');
+        $organization->setShortCode('UT');
+        $organization->setRedirectUrl('https://huwelijksplanner.online/betalen/betaald');
+        $manager->persist($organization);
+
+        $service = new Service();
+        $service->setType('mollie');
+        $service->setOrganization($organization)
+//        $service->
+        $manager->persist($service);
         $manager->flush();
     }
 }
