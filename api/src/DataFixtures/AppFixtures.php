b69c3893056d5e9fc049c56a8655e310f0f208e0
Robert Zondervan <rjzondervan@gmail.com>
2020-04-23 17:15:30 +0200

    fix fixtures

--- api/src/DataFixtures/AppFixtures.php
+++ api/src/DataFixtures/AppFixtures.php
@@ -5,25 +5,22 @@ namespace App\DataFixtures;
 use App\Entity\Organization;
 use App\Entity\Service;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
     private $params;
-    private $encoder;
 
-    public function __construct(ParameterBagInterface $params, UserPasswordEncoderInterface $encoder)
+    public function __construct(ParameterBagInterface $params)
     {
         $this->params = $params;
-        $this->encoder = $encoder;
     }
 
     public function load(ObjectManager $manager)
     {
         // Lets make sure we only run these fixtures on larping enviroment
-        if (strpos($this->params->get('app_domain'), "huwelijksplanner.online") == false && $this->params->get('app_domain' != 'huwelijksplanner.online')) {
+        if (strpos($this->params->get('app_domain'), "huwelijksplanner.online") == false && $this->params->get('app_domain') != 'huwelijksplanner.online') {
             return false;
         }
 
